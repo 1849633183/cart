ad9790ab6216be538bf75a1c44baf21180bd09d3
1849633183 <42697182+1849633183@users.noreply.github.com>
2019-05-27 16:59:20 +0800

    测试1

--- src/com/zb/utill/test.java
+++ src/com/zb/utill/test.java
@@ -36,7 +36,7 @@ public class test {
 		 try {
 			 Userinfo userinfo=mapper.findByUsername("1");
 			System.out.println(userinfo);
-			System.out.println("ttt");
+			System.out.println("tteeeet");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
