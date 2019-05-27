dbc2be2174695011e9a8a86f759a3ac86bc9b2d6
1849633183 <42697182+1849633183@users.noreply.github.com>
2019-05-27 17:38:33 +0800

    测试4

--- src/com/zb/utill/test.java
+++ src/com/zb/utill/test.java
@@ -36,7 +36,7 @@ public class test {
 		 try {
 			 Userinfo userinfo=mapper.findByUsername("1");
 			System.out.println(userinfo);
-			System.out.println("zb2");
+			System.out.println("zb4");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
